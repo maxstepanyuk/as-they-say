ba5ed252d692ea85337affe31ae4fcb07c20f0e5
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-02-15 03:32:24 +0200

    added 3 quotes

--- quotes.js
+++ quotes.js
@@ -49,4 +49,22 @@ const quotes = [
     //     date: "",
     //     place: "",
     // },
+    {
+        quote: "Приємного не багато",
+        name: "chichik (xl4tir)",
+        date: "2024_04_01",
+        place: "Telegram"
+    },
+    {
+        quote: "Добре що так можна в 2023",
+        name: "chichik (xl4tir)",
+        date: "2023_12_11",
+        place: "Telegram"
+    },
+    {
+        quote: "Це дахіття",
+        name: "chichik (xl4tir)",
+        date: "2023_03_01",
+        place: "Telegram"
+    },
 ];
