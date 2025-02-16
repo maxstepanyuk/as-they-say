b1e3c0d341bfde27eeb0805e689e4492eef42cc2
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-02-17 00:30:17 +0200

    moved quotes to quotesData.js

--- quotes.js
+++ quotes.js
@@ -32,39 +32,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
     container.innerHTML += "<hr>";
 });
-
-const quotes = [
-    // {
-    //     quote: "",
-    //     name: "",
-    //     date: "",
-    //     place: "",
-    // },
-    // {
-    //     quote: "",
-    //     dialogue: [
-    //         { name: "", line: "" },
-    //         { name: "", line: "" },
-    //     ],
-    //     date: "",
-    //     place: "",
-    // },
-    {
-        quote: "Приємного не багато",
-        name: "chichik (xl4tir)",
-        date: "2024_04_01",
-        place: "Telegram"
-    },
-    {
-        quote: "Добре що так можна в 2023",
-        name: "chichik (xl4tir)",
-        date: "2023_12_11",
-        place: "Telegram"
-    },
-    {
-        quote: "Це дахіття",
-        name: "chichik (xl4tir)",
-        date: "2023_03_01",
-        place: "Telegram"
-    },
-];
